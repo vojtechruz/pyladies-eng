15d0479ab0d94671139ec954472c4373b2b3e58f
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2022-06-16 17:55:37 +0200

    Classes - added some inheritance examples

---
+++ classes/classes/kitten1.py
@@ -0,0 +1,10 @@
+# Simple version of Kitten class with meow() method
+# All the kitten behave in the same way so far
+
+class Kitten:
+    def meow(self):
+        print('Kitten: Meow, I am a happy kitty!')
+
+
+kitty = Kitten()
+kitty.meow()
